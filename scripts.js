94649ac214a4fe571a46bd6b9400e1e07875492e
Pedroabreu155 <pedroliveirabreu@gmail.com>
2021-01-24 15:39:12 -0300

    cactus created, dino movemment done

--- scripts.js
+++ scripts.js
@@ -1,13 +1,19 @@
 const dino = document.querySelector('.dino')
+const background = document.querySelector('.background');
+let isJumping = false;
 
 function handleKeyUp(event) {
     if(event.keyCode === 32){
+        if(!isJumping){
         jump();
+        }  
     }
 };
 
 function jump(){
     let position = 0;
+
+    isJumping = true;
 
     let upInterval = setInterval(() => {
         if(position >= 150){
@@ -16,6 +22,7 @@ function jump(){
     let downInterval = setInterval(() => {
         if(position <= 0){
             clearInterval(downInterval);
+        isJumping = false;
         } else{
         position -= 20;
         dino.style.bottom = position + 'px';
@@ -28,5 +35,16 @@ function jump(){
         dino.style.bottom = position + 'px';
     }, 20);
 }
+
+
+function createCactus(){
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+    cactus.classList.add('cactus');
+    cactus.style.left = 1000 + 'px';
+    background.appendChild(cactus);
+}
+
+createCactus();
 
 document.addEventListener('keyup', handleKeyUp);
