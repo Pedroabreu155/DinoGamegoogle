47726eb57a2b63f34c2d1a6455daf1f7a8ade30a
Pedroabreu155 <pedroliveirabreu@gmail.com>
2021-01-24 16:42:11 -0300

    background and animation done

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const dino = document.querySelector('.dino')
 const background = document.querySelector('.background');
 let isJumping = false;
+let position = 0;
 
 function handleKeyUp(event) {
     if(event.keyCode === 32){
@@ -11,7 +12,6 @@ function handleKeyUp(event) {
 };
 
 function jump(){
-    let position = 0;
 
     isJumping = true;
 
@@ -31,7 +31,7 @@ function jump(){
 
         } else{}
         //subindo
-        position += 20;
+        position += 22;
         dino.style.bottom = position + 'px';
     }, 20);
 }
@@ -40,9 +40,29 @@ function jump(){
 function createCactus(){
     const cactus = document.createElement('div');
     let cactusPosition = 1000;
+    let randomTime = Math.random() * 6000;
+
     cactus.classList.add('cactus');
     cactus.style.left = 1000 + 'px';
     background.appendChild(cactus);
+
+
+    let leftInterval = setInterval( () => {
+
+        if(cactusPosition < -60){
+            clearInterval(leftInterval);
+            background.removeChild(cactus)
+        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+            // game over
+            clearInterval(leftInterval);
+            document.body.innerHTML = '<h1 class = "game-over">Game Over!</h1>';
+        } else{
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + 'px';
+        }
+    }, 20);
+
+    setTimeout(createCactus, randomTime);
 }
 
 createCactus();
